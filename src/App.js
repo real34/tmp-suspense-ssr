f584414f610ede02935a21ebc6b7e37661028f1a
Pierre Martin <pierre@occitech.fr>
2019-10-28 13:58:22 +0100

    simple app displaying current dishes in a restaurant

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const meat = { name: "Meat" };
+const pasta = { name: "Pasta" };
+const pie = { name: "Pie" };
+const salad = { name: "Salad" };
+
+const customers = [
+  {
+    name: "Pierre",
+    dishes: {
+      starter: null,
+      main: pasta,
+      dessert: pie
+    }
+  },
+  {
+    name: "Paul",
+    dishes: {
+      starter: salad,
+      main: pasta,
+      dessert: pie
+    }
+  },
+  {
+    name: "Jacques",
+    dishes: {
+      starter: null,
+      main: meat,
+      dessert: null
+    }
+  }
+];
+
+const Waiting = () => <p>Waiting for waiter or waitress</p>;
+const Dish = ({ dish }) => <p>{dish.name}</p>;
+
+const Customer = ({ name, currentDish, dishes }) => {
+  const dish = dishes[currentDish];
+
+  return (
+    <article>
+      <h2>{name}</h2>
+      {dish ? <Dish dish={dish} /> : <Waiting />}
+    </article>
+  );
+};
+
+const dishes = ["ordering", "starter", "main", "dessert"];
+
+const App = () => {
+  const [currentDish, setCurrentDish] = useState(dishes[0]);
+  const nextDish = dishes[dishes.indexOf(currentDish) + 1];
+
+  const handleNextDish = () => setCurrentDish(dish => nextDish);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,8 +72,16 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <h1>{currentDish}</h1>
+
+      {nextDish && <button onClick={handleNextDish}>Next dish</button>}
+
+      {customers.map((customer, index) => (
+        <Customer key={index} {...customer} currentDish={currentDish} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
