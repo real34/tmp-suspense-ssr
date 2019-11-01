b815a635c7fe6e95680808ddc339fd1344977249
Pierre Martin <pierre@occitech.fr>
2019-11-01 22:10:48 +0100

    allow several experimentations on the same page

--- src/App.js
+++ src/App.js
@@ -1,85 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-const meat = { name: "Meat" };
-const pasta = { name: "Pasta" };
-const pie = { name: "Pie" };
-const salad = { name: "Salad" };
-
-const customers = [
-  {
-    name: "Pierre",
-    dishes: {
-      starter: null,
-      main: pasta,
-      dessert: pie
-    }
-  },
-  {
-    name: "Paul",
-    dishes: {
-      starter: salad,
-      main: pasta,
-      dessert: pie
-    }
-  },
-  {
-    name: "Jacques",
-    dishes: {
-      starter: null,
-      main: meat,
-      dessert: null
-    }
-  }
-];
-
-const Waiting = () => <p>Waiting for waiter or waitress</p>;
-const Dish = ({ dish }) => <p>{dish.name}</p>;
-
-const Customer = ({ name, currentDish, dishes }) => {
-  const dish = dishes[currentDish];
-
-  return (
-    <article>
-      <h2>{name}</h2>
-      {dish ? <Dish dish={dish} /> : <Waiting />}
-    </article>
-  );
-};
-
-const dishes = ["ordering", "starter", "main", "dessert"];
+import Restaurant from "./Restaurant";
+import DataFetching from "./DataFetching";
 
 const App = () => {
-  const [currentDish, setCurrentDish] = useState(dishes[0]);
-  const nextDish = dishes[dishes.indexOf(currentDish) + 1];
-
-  const handleNextDish = () => setCurrentDish(dish => nextDish);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Simple React App with experiments of Concurrent mode features</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org/docs/concurrent-mode-intro.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read the docs!
         </a>
       </header>
 
-      <h1>{currentDish}</h1>
-
-      {nextDish && <button onClick={handleNextDish}>Next dish</button>}
-
-      {customers.map((customer, index) => (
-        <Customer key={index} {...customer} currentDish={currentDish} />
-      ))}
+      <div className="App-experiments">
+        <DataFetching />
+        <Restaurant />
+      </div>
     </div>
   );
 };
---
+++ src/DataFetching.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DataFetching = () => {
+  return (
+    <section>
+      <h1>Data fetching</h1>
+      TODO
+    </section>
+  );
+};
+
+export default DataFetching;
